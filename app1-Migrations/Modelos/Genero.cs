dcd4addd944ce6e74e49fc74865b812783cfcfd7
JosthinAyon <josthinayon@gmail.com>
2023-06-06 21:00:28 -0500

    ejemplo de migraciones

---
+++ app1-Migrations/Modelos/Genero.cs
@@ -0,0 +1,12 @@
+﻿namespace app1_Migrations.Modelos
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Boolean Status { get; set; }
+
+    }
+}
---
+++ app1-Migrations/Modelos/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace app1_Migrations.Modelos
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "VARCHAR(20)")]
+
+        public string Username { get; set; }
+        [Required]
+        [Column(TypeName = "VARCHAR(20)")]
+
+        public string Password { get; set; }
+        [Required]
+
+        public Boolean Status { get; set; }
+
+        public int GeneroId { get; set; }
+
+        public Genero Genero { get; set; }
+    }
+}
